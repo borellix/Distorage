a81251679ebb5bbc6136aed9b1c1e558512c4750
Sellig6792 <sellig.dev@gmail.com>
2022-04-30 19:18:51 +0200

    Correction de l'emplacement du nom de fichier

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,7 +5,7 @@ api = Blueprint('api', __name__)
 api.register_blueprint(v2, url_prefix='v2')
 
 # Last version
-api.register_blueprint(v2)
+api.register_blueprint(v2, name='current-version')
 @api.route('/')
 def index():
     return 'Welcome to the API'
